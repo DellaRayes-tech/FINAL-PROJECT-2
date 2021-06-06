d2272aa44229f03a078db9af4acb6df972d7e8ae
DellaRayes-tech <84915694+DellaRayes-tech@users.noreply.github.com>
2021-06-06 20:41:12 +0700

    Create della.js

---
+++ della.js
@@ -0,0 +1,26 @@
+function myFunction() {
+   var element = document.getElementsByClassName("top");
+   element[0].classList.toggle("dark-mode");
+   element = document.getElementsByClassName("container");
+   element[0].classList.toggle("dark-mode1");
+   element = document.getElementsByClassName("form-container");
+   element[0].classList.toggle("dark-mode");
+   element = document.getElementsByClassName("contact-form");
+   element[0].classList.toggle("dark-mode1");
+   element = document.getElementsByClassName("intro");
+   element[0].classList.toggle("dark-mode");
+   element = document.getElementsByClassName("karya-wrapper1");
+   element[0].classList.toggle("dark-mode");
+   element = document.getElementsByClassName("karya-wrapper2");
+   element[0].classList.toggle("dark-mode");
+   element = document.getElementsByClassName("pendidikan");
+   element[0].classList.toggle("dark-mode");
+   element = document.getElementsByClassName("kerja");
+   element[0].classList.toggle("dark-mode"); 
+   element = document.getElementsByTagName("input");
+   element[0].classList.toggle("dark-mode3");
+   element = document.getElementsByTagName("textarea");
+   element[0].classList.toggle("dark-mode3");
+   element = document.getElementsByTagName("body");
+   element[0].classList.toggle("filter");  
+}
